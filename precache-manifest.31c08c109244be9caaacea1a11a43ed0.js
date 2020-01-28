02b5373e3fa5eccfb8e0ef015aae53b6850b6cd2
Alekskarb <korbutowski@gmail.com>
2020-01-28 14:46:58 +0300

    Updates

---
+++ precache-manifest.31c08c109244be9caaacea1a11a43ed0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e697fc49edd1bda055ec3a6626400d35",
+    "url": "/Cost_Calculation_Project/index.html"
+  },
+  {
+    "revision": "d313dd2b7901d73c7ece",
+    "url": "/Cost_Calculation_Project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3cff7b42604f092d0b75",
+    "url": "/Cost_Calculation_Project/static/js/2.8d15bb00.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Cost_Calculation_Project/static/js/2.8d15bb00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d313dd2b7901d73c7ece",
+    "url": "/Cost_Calculation_Project/static/js/main.a176af36.chunk.js"
+  },
+  {
+    "revision": "7d1c98721dbc7243971b",
+    "url": "/Cost_Calculation_Project/static/js/runtime-main.dde3b05d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Cost_Calculation_Project/static/media/logo.5d5d9eef.svg"
+  }
+]);
