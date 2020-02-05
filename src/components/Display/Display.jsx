3549d23d1935fd32cb5657b463a46ff52af68c11
Alekskarb <korbutowski@gmail.com>
2020-02-05 14:28:28 +0300

    step Route

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -5,12 +5,13 @@ import FinishingWorksDisplay from "./FinishingWorksDisplay";
 import AdditionalWorksDisplay from "./AdditionalWorksDisplay";
 import CalculationDisplay from "./CalculationDisplay";
 import style from '../Display/Display.module.css'
+import {Route} from "react-router-dom";
 
 class Display extends React.Component {
     render() {
         return <div className={style.display}>
 
-                <StartDataDisplay/>
+        <Route component={StartDataDisplay}/>
                 <FinishingWorksDisplay/>
                 <AdditionalWorksDisplay/>
                 <CalculationDisplay/>
